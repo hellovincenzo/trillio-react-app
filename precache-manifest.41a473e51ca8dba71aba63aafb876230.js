126dad43bf50561222926116ebf91553446764ca
VinCodeTheWeb <vincenzo33.pellegrini@gmail.com>
2020-02-01 20:44:12 +0700

    Updates

---
+++ precache-manifest.41a473e51ca8dba71aba63aafb876230.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2834ba80cb00dae9e30f4c92326114",
+    "url": "/trillio-react-app/index.html"
+  },
+  {
+    "revision": "951d6da4f7ea1cd4ed70",
+    "url": "/trillio-react-app/static/css/main.770b008c.chunk.css"
+  },
+  {
+    "revision": "112e35723dd695d68102",
+    "url": "/trillio-react-app/static/js/2.edd5e118.chunk.js"
+  },
+  {
+    "revision": "497d7e07705261348096eb742962e0f0",
+    "url": "/trillio-react-app/static/js/2.edd5e118.chunk.js.LICENSE"
+  },
+  {
+    "revision": "951d6da4f7ea1cd4ed70",
+    "url": "/trillio-react-app/static/js/main.2ab83619.chunk.js"
+  },
+  {
+    "revision": "2d8c91138796d0512fd9",
+    "url": "/trillio-react-app/static/js/runtime-main.a8924c36.js"
+  },
+  {
+    "revision": "1c7c557e9714f393a4c7925e4a1c9840",
+    "url": "/trillio-react-app/static/media/aircraft-take-off.1c7c557e.svg"
+  },
+  {
+    "revision": "f20a50bb79803cf7dea48d545efd7834",
+    "url": "/trillio-react-app/static/media/bookmark.f20a50bb.svg"
+  },
+  {
+    "revision": "7e246b49875e3f5af2a3233f8dcbf822",
+    "url": "/trillio-react-app/static/media/chat.7e246b49.svg"
+  },
+  {
+    "revision": "0670a98b8aa1a7eb0a0c37f3cdfba06b",
+    "url": "/trillio-react-app/static/media/chevron-thin-right.0670a98b.svg"
+  },
+  {
+    "revision": "c7fedfa061fe1cdacbbfc58ca273898f",
+    "url": "/trillio-react-app/static/media/chevron-thin-right.c7fedfa0.svg"
+  },
+  {
+    "revision": "c5057c546f5632222015fcd1f9870fa6",
+    "url": "/trillio-react-app/static/media/home.c5057c54.svg"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/trillio-react-app/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/trillio-react-app/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/trillio-react-app/static/media/hotel-3.7af5efbc.jpg"
+  },
+  {
+    "revision": "d7d0212d8bc3c46656875534372e6e95",
+    "url": "/trillio-react-app/static/media/key.d7d0212d.svg"
+  },
+  {
+    "revision": "a68266a33ff4a9b2129e46d0184fa038",
+    "url": "/trillio-react-app/static/media/location-pin.a68266a3.svg"
+  },
+  {
+    "revision": "f9737d3ed2e6a8b07c15ff443e9d173d",
+    "url": "/trillio-react-app/static/media/magnifying-glass.f9737d3e.svg"
+  },
+  {
+    "revision": "df9b239dada5a12fc53049b2e6b040ae",
+    "url": "/trillio-react-app/static/media/map.df9b239d.svg"
+  },
+  {
+    "revision": "9c587a50aa216bea23193d446e3d7541",
+    "url": "/trillio-react-app/static/media/sprite.9c587a50.svg"
+  },
+  {
+    "revision": "7d0871a2719dfaa89abcdaf2a6d50c74",
+    "url": "/trillio-react-app/static/media/star.7d0871a2.svg"
+  },
+  {
+    "revision": "757b819974226076e3941b88a27b662e",
+    "url": "/trillio-react-app/static/media/text.757b8199.txt"
+  },
+  {
+    "revision": "a2a26d7906c257d584688e4a0130fe8c",
+    "url": "/trillio-react-app/static/media/user.a2a26d79.jpg"
+  }
+]);
